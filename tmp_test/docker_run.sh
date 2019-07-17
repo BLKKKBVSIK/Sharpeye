0cd31d2dda19a9c73b486e361efe5233200f0dbf
Baptiste Veyssiere <bv65@kent.ac.uk>
2019-07-17 11:54:36 +0200

    change tag to latest in docker_run script

--- tmp_test/docker_run.sh
+++ tmp_test/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 xhost +local:docker
-docker run --mount type=bind,source=$(pwd)/collision_detection,target=/playground/collision_detection -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -ti --rm hummingb1rd/opencv-contrib:1.0
+docker run --mount type=bind,source=$(pwd)/collision_detection,target=/playground/collision_detection -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -ti --rm hummingb1rd/opencv-contrib:latest
