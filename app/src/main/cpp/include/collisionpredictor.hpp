60598237b62c8f0c5e682383b027d6379fa518f3
Baptiste Veyssiere <bv65@kent.ac.uk>
2019-09-11 20:14:40 +0200

    add collision predictor include file forgotten in the previous commmit

---
+++ app/src/main/cpp/include/collisionpredictor.hpp
@@ -0,0 +1,28 @@
+#ifndef COLLISIONPREDICTOR_HPP
+#define COLLISIONPREDICTOR_HPP
+
+#include <map>
+#include <vector>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+
+class CollisionPredictor {
+public:
+	CollisionPredictor();
+	~CollisionPredictor();
+
+	bool alert(const std::map<int, cv::Rect2f> &objects, cv::Mat frame);
+
+private:
+	void add_size_to_history(const int id, const double size);
+	bool is_faster_than(const int id, cv::Rect2f box, cv::Mat frame);
+
+	std::map<int, std::vector<double>>	objects_size_history;
+	const int 							SAMPLE_SIZE = 30;
+	const int 							APX_THRESHOLD = 18;
+	const int 							GROWTH_THRESHOLD = 1;
+	const int 							MIN_X = 20;
+	const int 							MAX_X = 80;
+};
+
+#endif // COLLISIONPREDICTOR_HPP
