a0a60db26bc42ee9acd299e8594eabcad8d94568
Ilyas Semmaoui <ilyas.semmaoui@epitech.eu>
2019-10-13 22:11:34 +0200

    fix SharedPreferences

---
+++ app/src/main/java/sharpeye/sharpeye/SharpeyeApplication.java
@@ -0,0 +1,16 @@
+package sharpeye.sharpeye;
+
+import android.app.Application;
+import android.preference.PreferenceManager;
+
+public class SharpeyeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PreferenceManager.setDefaultValues(this, R.xml.pref_dangers, true);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_dashcam, true);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_signs, true);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_vocal, false);
+    }
+}
