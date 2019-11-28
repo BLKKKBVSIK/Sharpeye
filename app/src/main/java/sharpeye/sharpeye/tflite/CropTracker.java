12fc2dbbe4fd91cdfcb436801c51c873fc172f84
Nathan Scutari <nathan.scutari@epitech.eu>
2019-11-28 23:08:18 +0100

    cleanup

--- app/src/main/java/sharpeye/sharpeye/tflite/CropTracker.java
+++ app/src/main/java/sharpeye/sharpeye/tflite/CropTracker.java
@@ -17,12 +17,12 @@ public class CropTracker {
 
     private final static long TIME_LIMIT = 2000;
 
-    int width;
-    int height;
-    int cropSize;
-    int interval;
-    int limit;
-    int start;
+    private int width;
+    private int height;
+    private int cropSize;
+    private int interval;
+    private int limit;
+    private int start;
 
     boolean tracking = false;
     boolean idle = true;
